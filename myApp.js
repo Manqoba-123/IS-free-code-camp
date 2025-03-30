2b50eabc93354fbec46c166c7f3f89f45e468dff
Manqoba-123 <manoba231@gmail.com>
2025-03-30 12:28:19 +0200

    Updating type error issue

--- myApp.js
+++ myApp.js
@@ -48,19 +48,25 @@ const app = express();
 
 
 
-
-
-app.use(helmet());
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-
-
-app.use('/_api', (req, res, next) => {
+// Add a Router for /_api before freeCodeCamp code
+const apiRouter = express.Router();
+apiRouter.get('/', (req, res) => {
   res.json({ message: "Welcome to the API!" });
 });
+app.use('/_api', apiRouter); // Override before their line
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.use(helmet());
+
+module.exports = app; // freeCodeCamp line
+const api = require('./server.js'); // freeCodeCamp line
+app.use(express.static('public')); // freeCodeCamp line
+app.disable('strict-transport-security'); // freeCodeCamp line
+app.use('/_api', api); // freeCodeCamp line (this will override our Router)
+app.get("/", function (request, response) { // freeCodeCamp line
+  response.sendFile(__dirname + '/views/index.html'); // freeCodeCamp line
 });
-
-module.exports = app;
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`🔥🔥 
+The information securtiy App started on Port ${port} 🔥🔥`);
+});
