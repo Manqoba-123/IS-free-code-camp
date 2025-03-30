e9ead9a7ecb594ddcb679cc1ad7edebb448102a5
Manqoba-123 <manoba231@gmail.com>
2025-03-30 12:12:07 +0200

    Addressed the TypeError bug

--- myApp.js
+++ myApp.js
@@ -49,16 +49,18 @@ const app = express();
 
 
 
-module.exports = app;
-const api = require('./server.js');
+
+app.use(helmet());
 app.use(express.static('public'));
 app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+
+
+app.use('/_api', (req, res, next) => {
+  res.json({ message: "Welcome to the API!" });
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`🔥🔥 
-The information securtiy App started on Port ${port} 🔥🔥`);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+module.exports = app;
