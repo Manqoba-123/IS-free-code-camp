f123b3ca6b181c3caf077098c7064c336d8f4d32
Manqoba-123 <manoba231@gmail.com>
2025-03-30 12:40:38 +0200

    Removing commnted lines on the code

--- myApp.js
+++ myApp.js
@@ -48,22 +48,22 @@ const app = express();
 
 
 
-// Add a Router for /_api before freeCodeCamp code
+
 const apiRouter = express.Router();
 apiRouter.get('/', (req, res) => {
   res.json({ message: "Welcome to the API!" });
 });
-app.use('/_api', apiRouter); // Override before their line
+app.use('/_api', apiRouter);
 
 app.use(helmet());
 
-module.exports = app; // freeCodeCamp line
-const api = require('./server.js'); // freeCodeCamp line
-app.use(express.static('public')); // freeCodeCamp line
-app.disable('strict-transport-security'); // freeCodeCamp line
-app.use('/_api', api); // freeCodeCamp line (this will override our Router)
-app.get("/", function (request, response) { // freeCodeCamp line
-  response.sendFile(__dirname + '/views/index.html'); // freeCodeCamp line
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
